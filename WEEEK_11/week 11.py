f7b74775f028c3127c0c3c91ecdb9a2daa34addc
112007-m <mishkatenoor3@gmail.com>
2026-06-06 18:46:32 +0500

    Add files via upload

---
+++ WEEEK_11/week 11.py
@@ -0,0 +1,48 @@
+def hello_world():
+    print("hello world")
+hello_world()
+
+
+
+
+
+
+
+def information():
+    print("Name:mishkat e noor")
+information()
+
+
+
+
+
+
+name = "mishkat"
+print(type(name))
+
+num = 8
+print(type(num))
+
+decimal = 3.6
+print(type(decimal))
+
+num = int(input("Enter a number: "))
+print(type(num))
+
+
+
+
+
+
+
+
+def student_information(name):
+    print(f"Hi {name}.How old are you?")
+student_information("mishkat e noor")
+
+def information(name,registration_number):
+    print(f"Hi! My name is {name}.My registration number is {registration_number}?")
+information("def student_information(name):
+    print(f"Hi {name}.How old are you?")
+student_information("mishkat e noor",registeration number:5)
+
