47922272c2325372d9e78b2e35abd94735b88438
112007-m <mishkatenoor3@gmail.com>
2026-06-06 18:43:26 +0500

    Add files via upload

---
+++ WEEK_10/pf 1.py
@@ -0,0 +1,33 @@
+for i in range(4):
+    for j in range(4):
+        print("*",end=" ")
+
+
+for i in range(4):
+    for j in range (4):
+        print("*",end=" ")
+    print()
+
+
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+for i in range(j):
+    for j in range(1,5):
+        print("*",end=" ")
+    print()
+
+for j in range(2*i-1):
+     print("*",end=" ")
+print()
+
+
+for i in range(1,5):
+    for j in range(4-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print(" ",end=" ")
+    print()
+
---
+++ WEEK_10/task 1.py
@@ -0,0 +1,20 @@
+for i in range(4):
+    for j in range(4):
+        print("*",end=" ")
+
+
+for i in range(4):
+    for j in range (4):
+        print("*",end=" ")
+    print()
+
+
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+for i in range(j):
+    for j in range(1,5):
+        print("*",end=" ")
+    print()
