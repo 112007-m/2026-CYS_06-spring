fa40281aec53dc7564224a1fea7b6bee1912eb5b
112007-m <mishkatenoor3@gmail.com>
2026-06-06 18:42:31 +0500

    Add files via upload

---
+++ WEEK_06/WEEK 6.py
@@ -0,0 +1,45 @@
+
+
+
+#TASK 1
+def hello():
+    print("Hello World")
+    print("This is Mishkat")
+    print("Currently studying in UET")
+hello()
+
+
+#TASK 2
+def sum(x,y):
+    return x+y
+print(sum(11,12))
+print(sum(11,12))
+sum(11,12)
+
+
+
+#TASK 3
+def sum(a,b):
+    return a+b
+print(sum(11,12))
+
+
+#TASK 4
+c = 1
+d =2
+print(sum(c,d))
+
+
+len("Mishkat e noor")
+max(-20,20)
+
+
+#TASK 5
+x = double
+print(type(x))
+
+
+
+
+
+
---
+++ WEEK_09/task 3.py
@@ -0,0 +1,7 @@
+# )for i in range(1,3):
+#     for j in range(1,3):
+#         print("i= ",i,"j= ",j)
+for i in range(1,10):
+    for j in range(i):
+        print("*",end=" ")
+    print()
